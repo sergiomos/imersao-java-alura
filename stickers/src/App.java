05da22b6fa5e020aa22dd68cffc054f44a2b19df
Sérgio Martins <sergio.martins@take.net>
2022-07-18 11:03:17 -0300

    feat: create connection

--- stickers/src/App.java
+++ stickers/src/App.java
@@ -1,5 +1,22 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // Conexão http
+        String url = "https://imdb-api.com/en/Top250Movies/{apiKey}";
+        var client = HttpClient.newHttpClient(); 
+        var uri = URI.create(url);
+        var request = HttpRequest.newBuilder(uri).GET().build();
+        var response = client.send(request, BodyHandlers.ofString());
+        var body = response.body();
+        System.out.println(body);
+
+        // Extrair apenas as infos necessarias
+
+
     }
 }
